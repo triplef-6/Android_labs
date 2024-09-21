fe5956c586cf7f369b0de852a400dfaed6762818
yura <128141784+triplef-6@users.noreply.github.com>
2024-09-21 11:27:38 +0600

    Не работает

---
+++ HelloKitty/app/src/main/java/com/example/hellokitty/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.hellokitty
+
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mHelloTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        mHelloTextView = findViewById(R.id.textView)
+        var imageButton: ImageButton = findViewById(R.id.imageButton)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
