33e5b1a18fa5e34fdc8c8ca01bf9092f73b33c93
yura <128141784+triplef-6@users.noreply.github.com>
2024-10-05 11:11:12 +0600

    не работает

--- Trafficlight/app/src/main/java/com/example/trafficlight/MainActivity.kt
+++ Trafficlight/app/src/main/java/com/example/trafficlight/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.example.trafficlight
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*
+        не работает из-за переменных
+         */
+//        val rootLayout: ConstraintLayout = findViewById(R.id.main)
+//        val textView: TextView = findViewById(R.id.textView)
+//        val redButton: Button = findViewById(R.id.button_red)
+//        val yellowButton: Button = findViewById(R.id.button_yellow)
+//        val greenButton: Button = findViewById(R.id.button_green)
+
+//
+//        redButton.setOnClickListener {
+//            textView.setText(R.string.red)
+//            rootLayout.setBackgroundColor(resources.getColor(R.color.redColor, null))
+//        }
+//
+//        yellowButton.setOnClickListener {
+//            textView.setText(R.string.yellow)
+//            rootLayout.setBackgroundColor(resources.getColor(R.color.yellowColor, null))
+//        }
+//
+//        greenButton.setOnClickListener {
+//            textView.setText(R.string.green)
+//            rootLayout.setBackgroundColor(resources.getColor(R.color.greenColor, null))
+//        }
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
