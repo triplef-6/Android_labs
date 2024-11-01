4e9b4e547a374be65bcd68b46c098b5a211cda8b
yura <128141784+triplef-6@users.noreply.github.com>
2024-11-01 19:10:18 +0600

    Передача сообщения с одного экрана на другой

--- Threeactivities/app/src/main/java/com/example/threeactivities/MainActivity.kt
+++ Threeactivities/app/src/main/java/com/example/threeactivities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.threeactivities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,21 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+//        button.setOnClickListener {
+//            val intent = Intent(this@MainActivity, FirstActivity::class.java)
+//            startActivity(intent)
+//        }
         val button: Button = findViewById(R.id.button1)
+        val edit_address: TextView = findViewById(R.id.edit_address)
+        val edit_gift: TextView = findViewById(R.id.edit_gift)
+
         button.setOnClickListener {
-            val intent = Intent(this@MainActivity, FirstActivity::class.java)
-            startActivity(intent)
-        }
-        val button2: Button = findViewById(R.id.button2)
-        button2.setOnClickListener {
             val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            intent.putExtra("username", edit_address.text.toString())
+            intent.putExtra("gift", edit_gift.text.toString())
             startActivity(intent)
         }
+//        val button2: Button = findViewById(R.id.button2)
+//        button2.setOnClickListener {
+//            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
--- Threeactivities/app/src/main/java/com/example/threeactivities/SecondActivity.kt
+++ Threeactivities/app/src/main/java/com/example/threeactivities/SecondActivity.kt
@@ -1,12 +1,22 @@
 package com.example.threeactivities
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity: Activity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_second)
+//        val user = "ЖЫвотное"
+//        val gift = "дырку от бублика"
+        val user = intent.extras?.getString("username")
+        val gift = intent.extras?.getString("gift")
+
+        val textView: TextView = findViewById(R.id.textview_second_info)
+        textView.text = "$user, вам передали $gift"
     }
 }
