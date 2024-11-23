46b028f7f34b58977867b54fd1df654a4a0b9ce0
yura <128141784+triplef-6@users.noreply.github.com>
2024-11-23 15:00:30 +0600

    Меню готово

---
+++ Menu/app/src/main/java/com/example/menu/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.menu
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.menu.ui.theme.MenuTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+//        setContent {
+//            MenuTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val textView: TextView = findViewById(R.id.textView)
+        when (item.itemId) {
+            R.id.action_cat1 -> {
+                textView.text = "Вы выбрали кота!"
+                return true
+            }
+            R.id.action_cat2 -> {
+                textView.text = "Вы выбрали кошку!"
+                return true
+            }
+            R.id.action_cat3 -> {
+                textView.text = "Вы выбрали котёнка!"
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MenuTheme {
+        Greeting("Android")
+    }
+}
