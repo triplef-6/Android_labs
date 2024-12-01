460ae1fbbc54d6a1ff61ed20ec367eb6abac7b9e
yura <128141784+triplef-6@users.noreply.github.com>
2024-12-01 21:22:41 +0600

    Анимация есть

---
+++ Sunrise/app/src/main/java/com/example/sunrise/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.sunrise
+
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val sunImageView: ImageView = findViewById(R.id.sun)
+        val sunRiseAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.sun_rise)
+        sunImageView.startAnimation(sunRiseAnimation)
+    }
+}
