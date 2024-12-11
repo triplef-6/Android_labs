1db0be57fae4d12261139319f4f9034eb8709b44
yura <128141784+triplef-6@users.noreply.github.com>
2024-12-11 21:57:45 +0600

    Добавлена функция памяти

--- Crowcounter/app/src/main/java/com/example/crowcounter/MainActivity.kt
+++ Crowcounter/app/src/main/java/com/example/crowcounter/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.crowcounter
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -10,13 +12,18 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val APP_PREFERENCES_COUNTER = "counter"
     private var counter: Int = 0
+    private lateinit var prefs: SharedPreferences
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        prefs =
+            getSharedPreferences("settings", Context.MODE_PRIVATE)
+
         val textView: TextView = findViewById(R.id.textView)
 
         val button: Button = findViewById(R.id.button)
@@ -37,6 +44,25 @@ class MainActivity : AppCompatActivity() {
         val button_counter_clean: Button = findViewById(R.id.button_counter_clean)
         button_counter_clean.setOnClickListener {
             counter = 0
+            textView.text = "Я насчитал ${counter} ворон"
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        val editor = prefs.edit()
+        editor.putInt(APP_PREFERENCES_COUNTER, counter).apply()
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onResume() {
+        super.onResume()
+
+        if (prefs.contains(APP_PREFERENCES_COUNTER)) {
+            counter = prefs.getInt(APP_PREFERENCES_COUNTER, 0)
+
+            val textView: TextView = findViewById(R.id.textView)
             textView.text = "Я насчитал ${counter} ворон"
         }
     }
